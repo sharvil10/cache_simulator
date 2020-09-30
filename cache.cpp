10b80d324d58325eabd6572625f6e1af00b3c7c9
sharvil <shahsharvil96@gmail.com>
2020-09-29 23:38:29 -0400

    r/w added w/o inclusion policy and w/o testing

--- cache.cpp
+++ cache.cpp
@@ -34,8 +34,14 @@ Cache::Cache(uint32_t size, uint32_t block_size, uint32_t assoc)
         cout << block_bits <<endl;
     #endif
     tags = (uint32_t **) calloc(sets, sizeof(uint32_t *));
+    dirty = (char **) calloc(sets, sizeof(char *));
+    if(repl_pol == LRU)
+        sequence = (uint32_t **) calloc(sets, sizeof(uint32_t *));
     for(uint32_t i = 0; i < sets; i++)
         tags[i] = (uint32_t *) calloc(assoc, sizeof(uint32_t));
+        dirty[i] = (char *) calloc(assoc, sizeof(char));
+        if(repl_pol == LRU)
+            sequence[i] = (uint32_t *) calloc(assoc, sizeof(uint32_t));
 
 }
 
@@ -49,17 +55,134 @@ void Cache::add_below(Cache *below)
     this->below = below;
 }
 
-uint32_t Cache::get_tag(uint32_t address)
+static uint32_t Cache::get_tag(uint32_t address)
 {
    return (address && tag_mask) >> tag_shift;
 }
 
-uint32_t Cache::get_block(uint32_t address)
+static uint32_t Cache::get_block(uint32_t address)
 {
    return address && block_mask;
 }
 
-uint32_t Cache::get_index(uint32_t address)
+static uint32_t Cache::get_index(uint32_t address)
 {
-   return (address && index_mask) >> index_bits;
+   return (address && index_mask) >> block_bits;
+}
+
+static uint32_t Cache::convert_to_address(uint32_t tag, uint32_t index)
+{
+    uint32_t address = 0;//Offset of the block is irrelevant
+    address |= tag << tag_shift;
+    address |= index <<  block_bits;
+    return address;
+}
+
+void Cache::write(uint32_t address, uint32_t prog_counter)
+{
+    uint32_t tag = get_tag(address);
+    uint32_t index = get_index(address);
+    
+    for(uint32_t i=0; i < assoc; i++)
+    {
+        if((tag == tags[index][i]) && (dirty[index][i] != FREE))
+        {
+            //set_flags is used to distinguish between 
+            //0x00000000 and not set
+            hits++;
+            dirty[index][i] = DIRTY;//Write-back Policy
+            sequence[index][i] = prog_counter;
+            return;
+        }
+    }
+
+    //Cache miss so allocate
+    misses++;
+    allocate(address, tag, index);
+
+
+}
+
+void Cache::allocate(uint32_t address, uint32_t tag, uint32_t index, uint32_t prog_counter)
+{
+    for(uint32_t i=0; i < assoc; i++)
+    {
+        if(dirty[index][i] == FREE) //Free space found
+        {
+            dirty[index][i] = NOT_DIRTY;//Write-back Policy
+            tags[index][i] = tag;
+            sequence[index][i] = prog_counter;
+            return;
+        }
+    }
+
+    void replace(uint32_t tag, uint32_t index);
+    if(below == NULL)//Nothing below which means last cache so now will get from memory
+    {
+        mem_ops++;
+        return;
+    }
+    this->below->read(address);
+}
+
+void Cache::replace(uint32_t tag, uint32_t index)
+{
+    uint32_t idx;
+    switch(repl_pol)
+    {
+        case LRU:
+            idx = lru(tag, index);
+        case PSLRU:
+            return;
+        case OPT:
+            return;
+    }
+
+    if(dirty[index][idx] == DIRTY)//Write-back policy
+    {
+        uint32_t address_to_replace = convert_to_address(tag[index][idx], index);
+        if(below == NULL)
+            mem_ops++;
+        else
+            below->write(address_to_replace);
+    }
+    tags[index][idx] = tag;
+}
+
+uint32_t Cache::lru(uint32_t tag_to_write, uint32_t index)
+{
+    int32_t idx = 0;
+    uint32_t min = 0xffffffff;
+    for(uint32_t i=0; i < assoc; i++)
+    {
+        if(sequence[index][i] < min)
+        {
+            min = sequence[index][i];
+            idx = i
+        }
+    }
+    return idx;//Assoc index of the victim block
+
+}
+
+void Cache::read(uint32_t address, uint32_t prog_counter)
+{
+    uint32_t tag = get_tag(address);
+    uint32_t index = get_index(address);
+    
+    for(uint32_t i=0; i < assoc; i++)
+    {
+        if((tag == tags[index][i]) && (dirty[index][i] != FREE))
+        {
+            //set_flags is used to distinguish between 
+            //0x00000000 and not set
+            hits++;
+            sequence[index][i] = prog_counter;
+            return;
+        }
+    }
+
+    //Cache miss so allocate
+    misses++;
+    allocate(address, tag, index);
 }
--- cache.hpp
+++ cache.hpp
@@ -3,6 +3,17 @@
 #ifndef __CACHE_INLCUDED__
 #define __CACHE_INLCUDED__
 
+#define TRUE 0
+#define FALSE 1
+
+#define NOT_DIRTY 0
+#define DIRTY 1
+#define FREE 2
+
+#define LRU 0
+#define PLRU 1
+#define OPT 2
+
 using namespace std;
 
 class Cache
@@ -13,9 +24,13 @@ class Cache
         uint32_t block_size;
         uint32_t assoc;
         uint32_t sets;
-        uint32_t miss;
-        uint32_t hit;
+        uint32_t misses=0;
+        uint32_t hits=0;
+        uint32_t mem_ops=0;
         uint32_t **tags;
+        uint32_t **sequence; //For LRU
+        char **dirty; //Set if the specific tag is already used. 
+                          //To differentiate between 0x00000000 and not available.
         uint32_t tag_mask;
         uint32_t index_mask;
         uint32_t block_mask;
@@ -27,13 +42,15 @@ class Cache
 
         void add_above(Cache *above)
         void add_below(Cache *below)
-        void read(uint32_t address);
-        void write(uint32_t address);
-        uint32_t get_tag(uint32_t address);
-        uint32_t get_index(uint32_t address);
-        uint32_t get_block(uint32_t address);
+        void read(uint32_t address, uint32_t prog_counter);
+        void write(uint32_t address, uint32_t prog_counter);
+        void allocate(uint32_t address, uint32_t tag, uint32_t index, uint32_t prog_counter);
+        void replace(uint32_t tag, uint32_t index);
+        static uint32_t get_tag(uint32_t address);
+        static uint32_t get_index(uint32_t address);
+        static uint32_t get_block(uint32_t address);
     public:
-        Cache(uint32_t size, uint32_t block_size, uint32_t assoc);
+        Cache(uint32_t size, uint32_t block_size, uint32_t assoc, char repl_pol, char incl_pol);
 };
 
 #endif
